35fbf39b204f0e469219413f9fea44b2afbb367b
Daiki Shibata <yomogitablet@gmail.com>
2023-04-23 17:46:11 +0900

    add koro icon

--- vintage_diary/src/jsMain/kotlin/Main.kt
+++ vintage_diary/src/jsMain/kotlin/Main.kt
@@ -1,43 +1,31 @@
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import org.jetbrains.compose.web.css.padding
-import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.css.*
 import org.jetbrains.compose.web.dom.*
 import org.jetbrains.compose.web.renderComposable
-import org.w3c.dom.HTMLButtonElement
 
 fun main() {
-    var count: Int by mutableStateOf(0)
-
     renderComposable(rootElementId = "root") {
-        Div({ style { padding(25.px) } }) {
-            FloatingActionButton("remove") { onClick { count -= 1 } }
-
-            Span({ style { padding(15.px) } }) {
-                Text("$count")
-                Img(
-                    src = "https://github.com/DaiShiba/DaiShiba.github.io/blob/main/docs/koro.png"
-                )
+        Div( {
+            style {
+                padding(40.px)
+                display(DisplayStyle.Flex)
+                justifyContent(JustifyContent.Center)
+                flexDirection(FlexDirection.Column)
+                alignItems(AlignItems.Center)
+                width(100.vw)
+                height(100.vh)
+                background("#F1F1F1")
             }
-
-            FloatingActionButton("add") { onClick { count += 1 } }
-
-
+        }) {
+            Span {
+                Img(
+                    src = "https://github.com/DaiShiba/DaiShiba.github.io/blob/main/docs/koro.png?raw=true",
+                ) {
+                    style {
+                        width(60.px)
+                        height(60.px)
+                    }
+                }
+            }
         }
     }
-}
-
-@Composable
-fun FloatingActionButton(
-    icon: String,
-    attrs: AttrBuilderContext<HTMLButtonElement>? = null,
-) = Button(attrs = {
-    classes("btn-floating", "waves-effect", "waves-light")
-    attrs?.invoke(this)
-}) {
-    I(attrs = {
-        classes("material-icons")
-    }) { Text(icon) }
 }
