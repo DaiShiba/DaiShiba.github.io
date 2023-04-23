291066e16127164e00254c162b7ce7db774536ec
Daiki Shibata <yomogitablet@gmail.com>
2023-04-23 13:48:48 +0900

    init commit

---
+++ vintage_diary/src/jsMain/kotlin/Main.kt
@@ -0,0 +1,38 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.css.padding
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.*
+import org.jetbrains.compose.web.renderComposable
+import org.w3c.dom.HTMLButtonElement
+
+fun main() {
+    var count: Int by mutableStateOf(0)
+
+    renderComposable(rootElementId = "root") {
+        Div({ style { padding(25.px) } }) {
+            FloatingActionButton("remove") { onClick { count -= 1 } }
+
+            Span({ style { padding(15.px) } }) {
+                Text("$count")
+            }
+
+            FloatingActionButton("add") { onClick { count += 1 } }
+        }
+    }
+}
+
+@Composable
+fun FloatingActionButton(
+    icon: String,
+    attrs: AttrBuilderContext<HTMLButtonElement>? = null,
+) = Button(attrs = {
+    classes("btn-floating", "waves-effect", "waves-light")
+    attrs?.invoke(this)
+}) {
+    I(attrs = {
+        classes("material-icons")
+    }) { Text(icon) }
+}
